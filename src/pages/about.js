ecf27bc20c588004274d20c6987be5fd0cd4edd7
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2020-07-06 21:55:28 +0800

    Add typography plugin and add navigation to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
+// import Header from "../components/header"
 import Container from "../components/container"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
-    <div>
-      <Header />
-      About m<Link to="/contact/">contact</Link>
+    <Layout>
+      About <Link to="/contact/">contact</Link>
       <Container>
         <h1>About CSS Modules</h1>
         <p>CSS Modules are cool</p>
       </Container>
-    </div>
+    </Layout>
   )
 }
