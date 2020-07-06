e7295ecff49dc8d970bd93a9bcf762dc1ed6713b
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2020-07-06 21:28:21 +0800

    Add styles using css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Container from "../components/container"
 
 export default function About() {
   return (
     <div>
       <Header />
       About m<Link to="/contact/">contact</Link>
+      <Container>
+        <h1>About CSS Modules</h1>
+        <p>CSS Modules are cool</p>
+      </Container>
     </div>
   )
 }
